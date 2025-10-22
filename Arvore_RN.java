174251b869f277a4f300fcce3b1b4555cd913d06
deborahmacedo <deborah.macedo.104@ufrn.edu.br>
2025-10-22 11:45:50 -0300

    feat: Arvore_RN class

--- Arvore_RN.java
+++ Arvore_RN.java
@@ -1,5 +1,16 @@
-public class Arvore_RN extends Arvore_abstrata {
+public class Arvore_RN extends Arvore_abstrata<NoRN>{
 
+    @Override
+    public NoRN criar_no(int chave){
 
+    }
+    @Override
+    public void balancear(NoRN no) {
 
+    }
+
+    @Override
+    public void inserir(int chave) {
+        super.inserir(chave);
+    }
 }
