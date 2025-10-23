7e5c7276b790b5e8c3c3cd177a013c884c033ee3
Júlia Lima <julia.lima.713@ufrn.edu.br>
2025-10-23 20:24:38 -0300

    Implementação balancearNo
    Added comments to clarify height calculation and balancing cases.

--- arvore AVL.java
+++ arvore AVL.java
@@ -73,15 +73,17 @@ public class ArvoreAVL extends Arvore_abstrata<NoAVL> {
     }
 
      private int altura(NoAVL no) {
-        if (no == null) {
+        if (no == null) { // se o nó não existe
             return 0;
         } else {
             return no.altura;
         }
      }
 
+    // calcula altura do nó 
     private void novaAltura (NoAVL no) {
-        no.altura = 1 + Math.max(altura(no.esquerdo), altura(no.direito));
+        no.altura = 1 + Math.max(altura(no.esquerdo), altura(no.direito)); // retorna o maior valor entre a altura do filho esquerdo e a altura do filho direito. 
+        //maior valor = caminho mais longo  a partir do nó, e adiciona mais 1, pois o nó pai tá um nível acima
     }
     
  private int fatordeBalanceamento(NoAVL no) { // cálculo do balanceamento da AVL
@@ -90,4 +92,45 @@ public class ArvoreAVL extends Arvore_abstrata<NoAVL> {
         } else {
             return altura(no.esquerdo) - altura(no.direito);
         }
+    }
+
+    // balancear arvore 
+    private NoAVL balancearNo(NoAVL no) {
+        if (no == null) return null; // se o nó for vazio, retorna vazio
+
+        novaAltura(no); // atualiza a altura antes de calcular o FB
+        // garatindo que a altura esta certa para ver se está desbalanceada e qual caso usar
+        int fb = fatordeBalanceamento(no);
+
+        // CASO LL
+        // fb>1 : o nó  está desbalanceado para a esquerda
+        if (fb > 1 && fatordeBalanceamento(no.esquerdo) >= 0) {
+            return rotacao_direita(no); //o filho esquerdo sobe e se torna o novo pai da subárvore
+        }
+
+        // CASO LR
+        // desbalanceamento na subárvore direita do filho esquerdo do nó desbalanceado
+        if (fb > 1 && fatordeBalanceamento(no.esquerdo) < 0) {
+            no.esquerdo = rotacao_esquerda(no.esquerdo);
+            // atualiza pai após rotação
+            if (no.esquerdo != null) no.esquerdo.pai = no;
+            return rotacao_direita(no);
+        }
+
+        // CASO RR
+        // desbalanceamento na subárvore direita do filho direito do nó desbalanceado
+        if (fb < -1 && fatordeBalanceamento(no.direito) <= 0) {
+            return rotacao_esquerda(no);
+        }
+
+        // CASO RL
+        // desbalanceamento na subárvore esquerda do filho direito do nó desbalanceado
+        if (fb < -1 && fatordeBalanceamento(no.direito) > 0) {
+            no.direito = rotacao_direita(no.direito);
+            // atualiza pai depois da rotação
+            if (no.direito != null) no.direito.pai = no;
+            return rotacao_esquerda(no);
+        }
+
+        return no; // retorna o nó agora balanceado
     }
